9a0ce9e8bc6726ff8075e292a39104601f64e870
Akira Otaka <hydrangeas.project@gmail.com>
2023-12-22 15:32:20 +0900

    ログ出力を追加

--- AspireTests.API/Controllers/WeatherForecastController.cs
+++ AspireTests.API/Controllers/WeatherForecastController.cs
@@ -21,12 +21,15 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        _logger.LogInformation("GetWeatherForecast: called");
+        var data = Enumerable.Range(1, 50).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+        _logger.LogInformation($"GetWeatherForecast: data created");
+        return data;
     }
 }
